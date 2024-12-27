84826a7ff0ee9e4a02a12d116cbc712298cba316
action <xingdong1117@126.com>
2024-12-27 10:38:26 +0800

    feat:设计模式

--- demo/src/main/java/com/flyaction/demo/SpringELDemo.java
+++ demo/src/main/java/com/flyaction/demo/SpringELDemo.java
@@ -1,13 +1,15 @@
 package com.flyaction.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-public class DemoApplication {
+@Slf4j
+public class SpringELDemo {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		log.info("log:{}","abcd");
+		System.out.println();
 	}
 
 }
---
+++ demo/src/test/java/com/flyaction/demo/DesignPatternTest.java
@@ -0,0 +1,46 @@
+package com.flyaction.demo;
+
+import com.flyaction.demo.design.pattern.v2.Factory;
+import com.flyaction.demo.design.pattern.v2.HandlerStrategyFactory;
+import com.flyaction.demo.design.pattern.v3.AbstractColaHandler;
+import com.flyaction.demo.design.pattern.v3.CocaHandlerV3;
+import com.flyaction.demo.design.pattern.v3.FactoryV3;
+import com.flyaction.demo.design.pattern.v2.Factory;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @auther zzyy
+ * @create 2024-06-03 18:11
+ */
+@SpringBootTest
+public class DesignPatternTest
+{
+    /**
+     * 策略+工厂V2
+     */
+    @Test
+    public void methodV2()
+    {
+        String parameter = "Coca";
+
+        HandlerStrategyFactory invokeStrategy = Factory.getInvokeStrategy(parameter);
+
+        invokeStrategy.getCoca(parameter);
+    }
+
+    /**
+     * 策略+工厂+模板方法模式V3
+     */
+    @Test
+    public void methodV3()
+    {
+        String parameter = "Coca";
+        AbstractColaHandler handler = FactoryV3.getInvokeStrategy(parameter);
+
+        handler.getCoca(parameter);
+
+        System.out.println(handler.cocaMethod("可口可乐"));
+
+    }
+}
